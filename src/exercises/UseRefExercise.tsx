4e9ea821144fdcf93c25026f078944a78fba8c95
Samantha S <sami.berlynn@proton.me>
2025-04-02 22:18:33 +0200

    Finished UseRef exercise

--- src/exercises/UseRefExercise.tsx
+++ src/exercises/UseRefExercise.tsx
@@ -1,12 +1,22 @@
+import { useRef } from "react";
+
 function UseRefExercise() {
   // Add a button that allows the user to put focus on the input
   // use HTMLInputElement as useRef type
   // use the focus() method of the HTMLInputElement DOM node
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div>
       <h2>useRef Exercise</h2>
-      <input type="text" placeholder="Type something..." />
+      <input ref={inputRef} type="text" placeholder="Type something..." />
+      <button onClick={() => {
+        if(inputRef.current) {
+         inputRef.current.focus()
+        }
+      }
+      }>Click to Focus Input</button>
     </div>
   );
 }

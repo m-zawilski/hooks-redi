b616db1cf2c1cdbbc7fdcf48bace609c7656cc2b
Samantha S <sami.berlynn@proton.me>
2025-04-02 23:04:33 +0200

    Finished useContext Exercise

--- src/exercises/UseContextExercise.tsx
+++ src/exercises/UseContextExercise.tsx
@@ -1,53 +1,54 @@
 import { useState } from "react";
+import { useContext, createContext } from "react";
 
 // TODO: Refactor the code below to get rid of any props passing using useContext
 // Hint: use the interface and context creation below:
 
-// interface UserContext {
-//   user: User;
-//   setUser: (user: User) => void;
-// }
-// const UserContext = createContext<UserContext>({
-//   user: { name: "John Doe", age: 30 },
-//   setUser: () => {},
-// });
+interface UserContext {
+  user: User;
+  setUser: (user: User) => void;
+}
+const UserContext = createContext<UserContext>({
+  user: { name: "John Doe", age: 30 },
+  setUser: () => {},
+});
 
 interface User {
   name: string;
   age: number;
 }
 
-const Header = ({ user }: { user: User }) => {
+const Header = () => {
+  const userObj = useContext(UserContext);
+
   return (
     <header>
-      <h1>Welcome to the App, {user.name}!</h1>
+      <h1>Welcome to the App, {userObj.user.name}!</h1>
     </header>
   );
 };
 
-const UserProfile = ({ user }: { user: User }) => {
+const UserProfile = () => {
+  const userObj = useContext(UserContext);
+
   return (
     <div>
       <h2>User Profile</h2>
-      <p>Name: {user.name}</p>
-      <p>Age: {user.age}</p>
+      <p>Name: {userObj.user.name}</p>
+      <p>Age: {userObj.user.age}</p>
     </div>
   );
 };
 
-const UserSettings = ({
-  user,
-  setUser,
-}: {
-  user: User;
-  setUser: (user: User) => void;
-}) => {
+const UserSettings = () => {
+  const userObj = useContext(UserContext);
+
   const updateName = () => {
-    setUser({ ...user, name: "Jane Doe" });
+    userObj.setUser({ ...userObj.user, name: "Jane Doe" });
   };
 
   const updateAge = () => {
-    setUser({ ...user, age: 25 });
+    userObj.setUser({ ...userObj.user, age: 25 });
   };
 
   return (
@@ -59,17 +60,11 @@ const UserSettings = ({
   );
 };
 
-const MainContent = ({
-  user,
-  setUser,
-}: {
-  user: User;
-  setUser: (user: User) => void;
-}) => {
+const MainContent = () => {
   return (
     <main>
-      <UserProfile user={user} />
-      <UserSettings user={user} setUser={setUser} />
+      <UserProfile />
+      <UserSettings />
     </main>
   );
 };
@@ -79,8 +74,10 @@ const UseContextExercise = () => {
 
   return (
     <div>
-      <Header user={user} />
-      <MainContent user={user} setUser={setUser} />
+      <UserContext.Provider value={{user, setUser}}>
+        <Header />
+        <MainContent />
+      </UserContext.Provider>
     </div>
   );
 };
